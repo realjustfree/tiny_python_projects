c0e310bccea45801937608d244186e38d5935fed
luciano <realjustfree@gmail.com>
2023-10-01 18:55:10 +0900

    2023-10-01 18:54 chapter 6 further 문제 안품.

--- 06_wc/test.py
+++ 06_wc/test.py
@@ -27,7 +27,7 @@ def test_usage():
     """usage"""
 
     for flag in ['-h', '--help']:
-        rv, out = getstatusoutput(f'{prg} {flag}')
+        rv, out = getstatusoutput(f'python3 {prg} {flag}')
         assert rv == 0
         assert re.match("usage", out, re.IGNORECASE)
 
@@ -45,7 +45,7 @@ def test_bad_file():
     """bad_file"""
 
     bad = random_string()
-    rv, out = getstatusoutput(f'{prg} {bad}')
+    rv, out = getstatusoutput(f'python3 {prg} {bad}')
     assert rv != 0
     assert re.search(f"No such file or directory: '{bad}'", out)
 
@@ -54,7 +54,7 @@ def test_bad_file():
 def test_empty():
     """Test on empty"""
 
-    rv, out = getstatusoutput(f'{prg} {empty}')
+    rv, out = getstatusoutput(f'python3 {prg} {empty}')
     assert rv == 0
     assert out.rstrip() == '       0       0       0 ./inputs/empty.txt'
 
@@ -63,7 +63,7 @@ def test_empty():
 def test_one():
     """Test on one"""
 
-    rv, out = getstatusoutput(f'{prg} {one_line}')
+    rv, out = getstatusoutput(f'python3 {prg} {one_line}')
     assert rv == 0
     assert out.rstrip() == '       1       1       2 ./inputs/one.txt'
 
@@ -72,7 +72,7 @@ def test_one():
 def test_two():
     """Test on two"""
 
-    rv, out = getstatusoutput(f'{prg} {two_lines}')
+    rv, out = getstatusoutput(f'python3 {prg} {two_lines}')
     assert rv == 0
     assert out.rstrip() == '       2       2       4 ./inputs/two.txt'
 
@@ -81,7 +81,7 @@ def test_two():
 def test_fox():
     """Test on fox"""
 
-    rv, out = getstatusoutput(f'{prg} {fox}')
+    rv, out = getstatusoutput(f'python3 {prg} {fox}')
     assert rv == 0
     assert out.rstrip() == '       1       9      45 ../inputs/fox.txt'
 
@@ -90,7 +90,7 @@ def test_fox():
 def test_more():
     """Test on more than one file"""
 
-    rv, out = getstatusoutput(f'{prg} {fox} {sonnet}')
+    rv, out = getstatusoutput(f'python3 {prg} {fox} {sonnet}')
     expected = ('       1       9      45 ../inputs/fox.txt\n'
                 '      17     118     661 ../inputs/sonnet-29.txt\n'
                 '      18     127     706 total')
@@ -102,6 +102,6 @@ def test_more():
 def test_stdin():
     """Test on stdin"""
 
-    rv, out = getstatusoutput(f'{prg} < {fox}')
+    rv, out = getstatusoutput(f'python3 {prg} < {fox}')
     assert rv == 0
     assert out.rstrip() == '       1       9      45 <stdin>'

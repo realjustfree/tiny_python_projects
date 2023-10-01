61ec1f8220cfde4a380b52b26e0e40099414feeb
luciano <realjustfree@gmail.com>
2023-10-01 13:32:18 +0900

    2023-10-01 13:31 chap 4 끝. further 문제 덜 끝내긴 했으나 일단 통과.

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -13,37 +13,16 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Jump the Five',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('str',
                         metavar='str',
-                        help='A positional argument')
+                        help='Input text')
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
+    parser.add_argument('-t',
+                        '--text',
+                        help='change to text',
                         action='store_true')
 
     return parser.parse_args()
@@ -54,17 +33,29 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    str_arg = args.str
+    text_arg = args.text
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    trance_dial = {'1':'9', '2':'8', '3':'7','4':'6',
+                   '5':'0', '6':'4', '7':'3', '8':'2',
+                   '9':'1', '0':'5'}
+
+    trance_text = {'1':'One', '2':'two', '3':'three','4':'four',
+                   '5':'five', '6':'six', '7':'seven', '8':'eight',
+                   '9':'nine', '0':'zero'}
+
+    number=""
+    number2=[]
+
+    if text_arg:
+        print(str_arg.translate(str.maketrans(trance_text)))
+
+    else:
+        for char in str_arg:
+            if trance_dial.get(char):
+                print(trance_dial[char], end="")
+            else :
+                print(char, end="")
 
 
 # --------------------------------------------------

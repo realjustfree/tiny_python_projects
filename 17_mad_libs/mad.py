6b945f148efc77028b46ed132ad3d24158eae7db
lhs <realjustfree@gmail.com>
2023-10-24 19:56:11 +0900

    2023-10-24 19:55 chapter 17 시작

---
+++ 17_mad_libs/mad.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Author : Me <me@foo.com>
+Date   : today
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Mad Libs',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('FILE',
+                        type = argparse.FileType('rt'),
+                        metavar='FILE',
+                        help='Input file')
+
+    parser.add_argument('-i',
+                        '--input',
+                        help='Inputs (for testing)',
+                        nargs='*',
+                        type=str,
+                        default=None)
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    str_arg = args.arg
+    int_arg = args.int
+    file_arg = args.file
+    flag_arg = args.on
+    pos_arg = args.positional
+
+    print(f'str_arg = "{str_arg}"')
+    print(f'int_arg = "{int_arg}"')
+    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
+    print(f'flag_arg = "{flag_arg}"')
+    print(f'positional = "{pos_arg}"')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()

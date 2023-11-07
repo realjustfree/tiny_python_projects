09aad2d4d675adc074a954c260d52ec9a5ee0b50
lhs <realjustfree@gmail.com>
2023-11-07 19:06:39 +0900

    2023-11-07 19:06 chap 22 작성 중.

--- 22_itictactoe/itictactoe.py
+++ 22_itictactoe/itictactoe.py
@@ -1,4 +1,5 @@
 import random
+import re
 from typing import List, NamedTuple, Optional
 
 class State(NamedTuple):
@@ -9,12 +10,63 @@ class State(NamedTuple):
     error: Optional[str] = None
     winner: Optional[str] = None
 
-def format_board():
-    pass
+def format_board(board):
 
-def find_winner():
-    pass
+    cells = [str(i) if j=='.' else j for i, j in enumerate(board, start=1)]
+
+    line = "-"*13
+    board_tmpl = '| {} | {} | {} |'
+
+    return '\n'.join([
+        line,
+        board_tmpl.format(*cells[:3]),
+        line,
+        board_tmpl.format(*cells[3:6]),
+        line,
+        board_tmpl.format(*cells[6:]),
+        line
+    ])
+
+
+def find_winner(board):
+    winning = [
+        [0,1,2],[3,4,5], [6,7,8],
+        [0,3,6],[1,4,7], [2,5,8],
+        [0,4,8],[2,4,6]]
+
+    for player in 'OX':
+        for i, j, k in winning:
+            combo = [board[i], board[j], board[k]]
+            if combo == [player, player, player]:
+                return player
+
+
 def main():
+    state = State()
+
+    while True:
+        board_now = state.board
+
+        print(format_board(board_now))
+        board_num = input(f'Player {state.player}, what is your move? [q to quit] : ')
+
+        if board_num == 'q':
+            break
+        if re.search('[0-9]', board_num):
+            board_now[int(board_num)-1] = state.player
+
+            state = state._replace(board=board_now, player=change_player(state.player))
+
+
+def change_player(player):
+    if player == 'X':
+        return 'O'
+    else:
+        return 'X'
+
+
+
+
 
 
 if __name__ == '__main__':
